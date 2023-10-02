14dff7a2c14ddb076273f54a6f7f2ba5e1641c25
Benjamin Morledge-Hampton <brotherbenben@yahoo.com>
2023-10-02 15:59:41 -0700

    Overhaul for MassRename
    - Overhaul inspired by Python Working Group problem of the month!
    - Directory tree traversal now happens from the bottom up using os.walk.
    - Directories can be renamed in addition to files.
    - Renaming can be case-insensitive.

--- Python/MassRename.py
+++ Python/MassRename.py
@@ -4,52 +4,69 @@
 
 import os
 from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog
-from mutperiodpy.helper_scripts.UsefulFileSystemFunctions import getDataDirectory
 
 
-def massRename(parentDirectory, searchString: str, replaceString: str, verbose = False):
+def massRename(parentDirectory, searchString: str, replaceString: str, caseInsensitive = False, renameDirectories = False,
+               verbose = False):
+
+    # Define a generic function for renaming a path, if appropriate.
+    def attemptPathRename(directory: str, basename: str):
+
+        # Check to see if the search string is in the filename.
+        # Whether or not this check is case sensitive is determined by the relevent parameter.
+        if caseInsensitive: searchStringFound = searchString.lower() in basename.lower()
+        else: searchStringFound = searchString in basename
+
+        # Check for the search string, and replace it if found.
+        if searchStringFound:
+
+            # Generate a new file name by replacing the relevant strings.
+            # Case sensitivity counts here too.
+            if verbose: print(f"Found item with search string: {basename}")
+            if caseInsensitive: newBasename = basename.lower().replace(searchString.lower(), replaceString)
+            else: newBasename = basename.replace(searchString, replaceString)
+            if verbose: print(f"Replacing with new name: {newBasename}")
+
+            # Generate the old and new file paths.
+            oldPath = os.path.join(directory, basename)
+            newPath = os.path.join(directory, newBasename)
+
+            try:
+                os.rename(oldPath, newPath)
+                # NOTE: I think there was a WSL bug before that made it difficult to rename files that only changed case.
+                #       Not sure if I need to implement a fix for that again though or if it can be circumvented by
+                #       ensuring this script runs in the OS's native file system.
+            except PermissionError:
+                print(f"WARNING: Permission denied at {oldPath}\nUnable to rename.")
 
     assert searchString, "Search string cannot be empty."
 
     # Iterate through the given directory
-    for item in os.listdir(parentDirectory):
-        path = os.path.join(parentDirectory,item)
+    for dirPath, _, fileNames in os.walk(parentDirectory, topdown=False):
+        
+        if verbose: print(f"\nWorking in {dirPath}")
 
-        # Recursively search directories
-        if os.path.isdir(path): 
-            if verbose: print(f"Recursing into directory: {item}")
-            massRename(path, searchString, replaceString)
+        # Iterate through all the files in the current directory.
+        for fileName in fileNames: attemptPathRename(dirPath, fileName)
 
-        # Check for the search string, and replace it if found.
-        if searchString in item:
-
-            if verbose: print(f"Found item with search string: {item}")
-            newItem = item.replace(searchString, replaceString)
-            if verbose: print(f"Replacing with new name: {newItem}")
-
-
-            try:
-                # If all that's changing is case, we need to do an intermediate renaming (IDK, it just works this way).
-                if searchString.lower() == replaceString.lower():
-                    intermediateItem = item.replace(searchString, replaceString + 'BeN_wUz_HeRe')
-                    os.rename(path, os.path.join(parentDirectory, intermediateItem))
-                    os.rename(os.path.join(parentDirectory, intermediateItem), os.path.join(parentDirectory, newItem))
-                else: os.rename(path, os.path.join(parentDirectory, newItem))
-            except PermissionError:
-                print(f"WARNING: Permission denied at {path}\nUnable to rename.")
-
+        # Repeat the process for the directory, if desired.
+        if renameDirectories: attemptPathRename(os.path.dirname(dirPath), os.path.basename(dirPath))
 
 
 def main():
 
     #Create the Tkinter UI
-    with TkinterDialog(workingDirectory=getDataDirectory()) as dialog:
+    with TkinterDialog(workingDirectory=os.path.abspath(os.path.join(__file__,"..","..",".."))) as dialog:
         dialog.createFileSelector("Parent Directory:", 0, directory = True)
         dialog.createTextField("String to search for:", 1, 0, defaultText='')
         dialog.createTextField("String to replace with:", 2, 0, defaultText='')
+        dialog.createCheckbox("Case insensitive:", 3, 0)
+        dialog.createCheckbox("Rename directories:", 4, 0)
+        dialog.createCheckbox("Verbose:", 5, 0)
 
     selections = dialog.selections
     massRename(selections.getIndividualFilePaths()[0], selections.getTextEntries()[0],
-               selections.getTextEntries()[1])
+               selections.getTextEntries()[1], selections.getToggleStates()[0],
+               selections.getToggleStates()[1], selections.getToggleStates()[2])
 
 if __name__ == "__main__": main()
